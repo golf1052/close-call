540fffbdc23d415c4eac52d4ca5d5317b2386d80
knuevena <andrew@weft.io>
2016-01-23 15:16:21 -0500

    no mongo for now. schedule stub

--- core/core.py
+++ core/core.py
@@ -6,20 +6,18 @@ import pymongo
 import config
 
 scheduler = Scheduler(connection=Redis())
-client = pymongo.MongoClient("mongodb://" + config.MONGO_USER + config.MONGO_PW + "@" + MONGO_IP)
-db = client.admin
+
+def connect_to_mongo():
+    client = pymongo.MongoClient("mongodb://" + config.MONGO_USER + config.MONGO_PW + "@" + MONGO_IP)
+    db = client[db]
+    return db
 
 
-def schedule(time, token, module, number):
-    #something like this. the module doing the calling has to deal with results
-    new_id = scheduler.enqueue_at(strptime(time), twilio.call, token, name) # Date time should be in UTC
-    #this is still a dumb schema
-    db.closecall.users.update(
-        {"phone_number": number},
-        {"$set": {"job_id": new_id}},
-    )
+def schedule(time, consequence, number):
+    #something like this. The syntax is time, method, args
+    scheduler.enqueue_at(strptime(time), twilio.call, number, consequence) # Date time should be in UTC
+
 
 def unschedule(number):
-    user = db.closecall.users.find({"phone_number": number})
     if user.job_id in scheduler.get_jobs():
         scheduler.cancel(user.job_id)

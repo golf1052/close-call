51a9c21f402571d83d546680bddeb1e335460f91
Tevin Otieno <motevin@gmail.com>
2016-01-23 14:54:55 -0500

    schema changes & added last_date

--- consequences/facebook.py
+++ consequences/facebook.py
@@ -21,26 +21,46 @@ def connect_to_mongo():
 def get_access_token(phone):
     db = connect_to_mongo()
     global access_token
-    access_token_dict = db.users.find_one({'phone_number': phone}, {'access_tokens.facebook': 1, '_id': 0})
-    access_token = access_token_dict['access_tokens']['facebook']
+    access_token_dict = db.users.find_one({'phone_number': phone}, {'consequences.facebook.access_token': 1, '_id': 0})
+    access_token = access_token_dict['consequences']['facebook']['access_token']
     print access_token
-    get_old_post()
+    get_old_post(phone)
 
-# Get random old facebook post
-def get_old_post():
+# Get last_date used
+def get_last_date(phone):
+    db = connect_to_mongo()
+    last_date_dict = db.users.find_one({'phone_number': phone}, {'consequences.facebook.last_date': 1, '_id': 0})
+    last_date = last_date_dict['consequences']['facebook']['last_date']
+    print 'Last date : ' + str(last_date)
+    return last_date
+
+# Update the last_date used for getting a user's post
+def update_last_date(phone, date):
+    return date
+
+# Get earliest unused facebook post
+def get_old_post(phone):
     global access_token
+    last_date = get_last_date(phone)
+    if last_date is None:
+        print "No last date found, resetting to 0"
+        last_date = 0
     query = 'SELECT post_id, message, permalink, created_time ' \
         'FROM stream ' \
         'WHERE source_id = me() ' \
         'AND is_hidden != \"true\" ' \
         'AND type = 46 ' \
+        'AND created_time > ' + str(last_date) + ' ' \
         'ORDER BY created_time ASC ' \
-        'LIMIT 2'
-    params = urllib.urlencode({'q':query, 'access_token':access_token})
-    response = requests.get('https://graph.facebook.com/v2.0/fql?' + params)
-    print response.json()
+        'LIMIT 5'
+    params = urllib.urlencode({'q': query, 'access_token': access_token})
+    response_json = requests.get('https://graph.facebook.com/v2.0/fql?' + params).json()
+    post_dict = {'post_id': response_json['data'][0]['post_id'], 'post': response_json['data'][0]['message']}
+    print post_dict
 
-    return
+# Share post
+def share_post_using_id(post_id):
+    
 def main():
     get_access_token("9145632336")
 

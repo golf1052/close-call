580b6ed103d95ca3581df8367fda95679303b7cc
knuevena <andrew@weft.io>
2016-01-23 05:17:25 -0500

    just update job

--- core/core.py
+++ core/core.py
@@ -3,27 +3,21 @@ from rq import Queue
 from rq_scheduler import Scheduler
 from datetime import datetime
 from pymongo import mongo
+import config
 
 scheduler = Scheduler(connection=Redis())
 client = pymongo.MongoClient("mongodb://" + config.MONGO_USER + config.MONGO_PW + "@" + MONGO_IP)
 db = client.admin
 
 
-def schedule(time, token, module, name):
+def schedule(time, token, module, number):
     #something like this. the module doing the calling has to deal with results
     new_id = scheduler.enqueue_at(strptime(time), twilio.call, token, name) # Date time should be in UTC
-    #this is a dumb schema
-    db.users.insert(
-        {
-        user_id: name,
-        job_details: {
-            token_type: module,
-            secret_token: token,
-            call_time: time,
-            job_id: new_id
-            }
-        }
-    )
+    #this is still a dumb schema
+    db.closecall.users.update(
+      { "phone_number" : number },
+      { $set: { "job" : new_id  }},
+      )
 
 def unschedule(key):
     #get jobid from mongo using key/token?
